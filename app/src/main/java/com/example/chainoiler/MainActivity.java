b9ee70d0fd0db14d68ec2a6f94d85ce58d4280ec
Perivolaris Nicolas <zigandzag@hotmail.be>
2021-06-24 08:23:08 +0200

    GUI v1

--- app/src/main/java/com/example/chainoiler/MainActivity.java
+++ app/src/main/java/com/example/chainoiler/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.chainoiler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activatesModes(false);
+        activatesButton(false);
+
+
+    }
+
+    private void activatesButton(boolean b) {
+        ((findViewById(R.id.sprayBt))).setEnabled(b);
+        ((findViewById(R.id.continuousBt))).setEnabled(b);
+        ((findViewById(R.id.drop1Bt))).setEnabled(b);
+    }
+
+    public void onModeEdit(View view){
+        boolean check = (((SwitchCompat)view).isChecked());
+        activatesModes(check);
+    }
+
+    public void onUnlock(View view){
+        Button lock = (Button)view;
+        if(lock.getText().equals(getResources().getString(R.string.lock))){
+            lock.setText(R.string.unlock);
+            findViewById(R.id.buttonsTL).setBackgroundResource(R.drawable.caution);
+        }
+        else{
+            lock.setText(R.string.lock);
+            findViewById(R.id.buttonsTL).setBackground(null);
+        }
+        activatesButton(lock.getText().equals(getResources().getString(R.string.unlock)));
+    }
+
+    private void activatesModes(boolean check){
+        RadioGroup modes = ((RadioGroup)findViewById(R.id.modeRG));
+        for(int i = 0; i < modes.getChildCount(); i++)
+            modes.getChildAt(i).setEnabled(check);
     }
 }
